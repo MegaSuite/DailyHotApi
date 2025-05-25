6a82b91d460db32cdbc52a7709c7ac677f94c4fa
konrad <amazing0possible@gmail.com>
2025-05-25 14:55:11 +0800

    multidays for zhihu-daily

--- src/routes/zhihu-daily.ts
+++ src/routes/zhihu-daily.ts
@@ -17,27 +17,60 @@ export const handleRoute = async (_: undefined, noCache: boolean) => {
 };
 
 const getList = async (noCache: boolean) => {
-  const url = `https://daily.zhihu.com/api/4/news/latest`;
-  const result = await get({
-    url,
-    noCache,
-    headers: {
-      Referer: "https://daily.zhihu.com/api/4/news/latest",
-      Host: "daily.zhihu.com",
-    },
-  });
-  const list = result.data.stories.filter((el: RouterType["zhihu-daily"]) => el.type === 0);
-  return {
-    ...result,
-    data: list.map((v: RouterType["zhihu-daily"]) => ({
-      id: v.id,
-      title: v.title,
-      cover: v.images?.[0] ?? undefined,
-      author: v.hint,
-      hot: undefined,
-      timestamp: undefined,
-      url: v.url,
-      mobileUrl: v.url,
-    })),
-  };
+  try {
+    // Get the latest data
+    const url = `https://daily.zhihu.com/api/4/news/latest`;
+    const result = await get({
+      url,
+      noCache,
+      headers: {
+        Referer: "https://daily.zhihu.com/api/4/news/latest",
+        Host: "daily.zhihu.com",
+      },
+    });
+
+    const list = result.data.stories.filter((el: RouterType["zhihu-daily"]) => el.type === 0);
+
+    // Get the previous day's data
+    let date = result.data.date;
+    const year = parseInt(date.substring(0, 4), 10);
+    const month = parseInt(date.substring(4, 6), 10) - 1;
+    const day = parseInt(date.substring(6, 8), 10);
+
+    const previousDate = new Date(year, month, day);
+    previousDate.setDate(previousDate.getDate() - 1);
+    const previousDateString = previousDate.toISOString().split("T")[0].replace(/-/g, "");
+
+    const beforeURL = `https://daily.zhihu.com/api/4/news/before/${previousDateString}`;
+    const beforeResult = await get({
+      url: beforeURL,
+      noCache,
+      headers: {
+        Referer: beforeURL,
+        Host: "daily.zhihu.com",
+      },
+    });
+
+    const beforeList = beforeResult.data.stories.filter((el: RouterType["zhihu-daily"]) => el.type === 0);
+    // Combine the two lists
+    const combinedList = list.concat(beforeList);
+
+    return {
+      fromCache: result.fromCache,
+      updateTime: result.updateTime,
+      data: combinedList.map((v: RouterType["zhihu-daily"]) => ({
+        id: v.id,
+        title: v.title,
+        cover: v.images?.[0] ?? undefined,
+        author: v.hint,
+        hot: undefined,
+        timestamp: undefined,
+        url: v.url,
+        mobileUrl: v.url,
+      })),
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
